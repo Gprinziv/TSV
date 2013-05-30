5cc259ed1996b18557d900eb9c82d154c36bcb87
Andrew Guenther <guenther.andrew.j@gmail.com>
2013-05-30 04:35:37 -0700

    Added leginfo scripts to repo

---
+++ sql/leginfo/opengov_load.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+DATA=/tmp/leginfo_load
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+read -s -p "Enter SQL root password: " sqlpass
+echo
+
+mkdir $DATA 2> /dev/null
+
+for fold in $@
+do
+   echo "$fold"
+
+   if [ ! -e $fold ]; then
+      wget ftp://www.leginfo.ca.gov/pub/bill/$fold.zip
+      unzip $fold.zip -d $fold -q
+      rm $fold.zip
+   fi
+
+   echo "Moving bill version data"
+
+   rm $DATA/* 2> /dev/null
+   cp $fold/BILL_VERSION*.lob $DATA
+
+   echo "Applying bill version data permissions"
+
+   chown -R mysql $DATA
+   chmod -R o+r $DATA
+
+   for TBL in opengov_load/*
+   do
+      echo "   Loading ${TBL%.*}..."
+      (cd $fold && mysql -uroot -p$sqlpass --local-infile=1 -Dcapublic -f -v < $DIR/$TBL)
+   done
+done
