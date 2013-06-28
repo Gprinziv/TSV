07d533559327485500faa9605ef05de51305dce7
kim <kim@zues.cadrc.calpoly.edu>
2013-06-28 00:13:50 -0700

    moved tons of stuff and added db connection to search -- problem with play framework and lucene that we have not resolved

--- app/controllers/Search.java
+++ app/controllers/Search.java
@@ -4,6 +4,11 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.search;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.Date;
+import search.Indexer;
+
 public class Search extends Controller {
 
    public static Result search(String query) {
@@ -11,6 +16,35 @@ public class Search extends Controller {
    }
    
    public static Result index() {
+      //Create indexers (one for singluar transcript), one for all leg docs
+      Indexer transndx = new Indexer("../../data/indexes/transndx");
+      Indexer legndx = new Indexer("../../data/indexes/legndx");
+
+      Hearing hear = Hearing.getOne(new Date(0,0,0), "201120120SB1530", 0);
+      List<Utterance> utterances = hear.getTranscript();
+      List<Bill> bills = Bill.getMany(0, Integer.MAX_VALUE);
+            
+      //index the transcript in it's own index (for prototype only!)
+      transndx.indexUtterance(utterances);
+
+      List<Listable> docs = new ArrayList<Listable>();
+      Listable docu;
+      
+      for (int i=0; i < utterances.size(); i++) {
+         docu = utterances.get(i);
+         docs.add(docu);
+      }
+
+      for (int i=0; i < bills.size(); i++) {
+         docu = bills.get(i);
+         docs.add(docu);
+      }
+
+      docs.add((Listable)hear);
+      
+      //index all documents
+      legndx.indexUtterance(docs);
+  
       return ok();
    }
 }
