107cbbc3c9623f4cb4a34ed35d6c166fdc8e014d
Andrew Guenther <guenther.andrew.j@gmail.com>
2013-06-27 22:35:57 -0700

    Made Utterance listable

--- app/models/Utterance.java
+++ app/models/Utterance.java
@@ -1,6 +1,6 @@
 package models;
 
-public class Utterance {
+public class Utterance implements Listable {
    String body;
    Integer time;
    Legislator speaker;
@@ -9,5 +9,20 @@ public class Utterance {
       this.body = body;
       this.time = time;
       this.speaker = speaker;
+   }
+
+   @Override
+   public String getHeading() {
+      return speaker.getFirst() + " " + speaker.getLast();
+   }
+
+   @Override
+   public String getDescription() {
+      return body;
+   }
+
+   @Override
+   public String getLink() {
+      return null;
    }
 }
