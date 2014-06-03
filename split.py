16642a05314d0ef3c5cfa4074dd0581ab026c787
Gprinziv <gprinziv@gmail.com>
2014-06-03 03:21:30 -0700

    Next version

--- split.py
+++ split.py
@@ -10,10 +10,11 @@ files = []
 with open(sys.argv[1], 'rb') as csvfile:
     formreader = csv.reader(csvfile, delimiter='\t')
     h = formreader.next()
+    with open("headers.csv", "wb") as wf:
+        temp = csv.writer(wf)
+        temp.writerow((h[3], h[4], h[5], h[6], h[7], h[8], h[12], h[13], h[14], h[61], h[1], h[15], h[16]))
+
     for r in formreader:
         with open("Form" + r[3] + ".csv", "ab") as wf:
             temp = csv.writer(wf)
-            if (files.count(r[3]) == 0):
-                files.append(r[3])
-                temp.writerow((h[3], h[4], h[5], h[6], h[7], h[8], h[12], h[13], h[14], h[61], h[1], h[15], h[16]))
             temp.writerow((r[3], r[4], r[5], r[6], r[7], r[8], r[12], r[13], r[14], r[61], r[1], r[15], r[16]))
